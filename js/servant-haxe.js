ab1f5df0faf03e4bb27e47c6a577070a6852df8c
Eduardo Dias da Costa <eduardo@thelaborat.org>
2016-03-30 12:22:42 -0300

    project

--- js/servant-haxe.js
+++
@@ -1,7 +0,0 @@
-(function (console) { "use strict";
-var Main = function() { };
-Main.main = function() {
-	js.suit.core.Servant.start();
-};
-Main.main();
-})(typeof console != "undefined" ? console : {log:function(){}});
